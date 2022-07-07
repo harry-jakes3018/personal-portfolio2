9bb14455674309277be57caa30439eb4ea5909c5
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-07-07 18:15:43 +0530

    Skills and Slider Section Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Banner from "./components/Banner";
+import Skills from "./components/Skills";
 
 function App() {
     return (
         <div>
             <NavBar />
             <Banner />
+            <Skills />
         </div>
     );
 }
