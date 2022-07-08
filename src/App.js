5aa00e1186d15385126305ebdcdf9230f7c05193
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-07-08 16:00:53 +0530

    Contact Form Section Completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Banner from "./components/Banner";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
             <Banner />
             <Skills />
             <Projects />
+            <Contact />
         </div>
     );
 }
