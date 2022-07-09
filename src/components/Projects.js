5c442ab0563c947feb97dc118b40e526a39bfc7d
Jitendra Garg <107532341+harry-jakes3018@users.noreply.github.com>
2022-07-09 20:50:07 +0530

    Animation Completed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,8 @@ import colorSharp2 from "../assets/img/color-sharp2.png";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 function Projects() {
     const projects = [
@@ -49,14 +51,28 @@ function Projects() {
             <Container>
                 <Row>
                     <Col>
-                        <h2>Projects</h2>
-                        <p>
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry's standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and
-                            scrambled it to make a type specimen book.
-                        </p>
+                        <TrackVisibility>
+                            {({ isVisible }) => (
+                                <div
+                                    className={
+                                        isVisible
+                                            ? "animate__animated animate__slideInUp"
+                                            : ""
+                                    }
+                                >
+                                    <h2>Projects</h2>
+                                    <p>
+                                        Lorem Ipsum is simply dummy text of the
+                                        printing and typesetting industry. Lorem
+                                        Ipsum has been the industry's standard
+                                        dummy text ever since the 1500s, when an
+                                        unknown printer took a galley of type
+                                        and scrambled it to make a type specimen
+                                        book.
+                                    </p>
+                                </div>
+                            )}
+                        </TrackVisibility>
                         <Tab.Container
                             id="project-tabs"
                             defaultActiveKey="first"
